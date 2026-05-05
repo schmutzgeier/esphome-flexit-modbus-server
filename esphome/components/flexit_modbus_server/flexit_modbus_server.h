db0c87837f60d0f14a08347693bc37c10432cc6c
schmutzgeier <132825726+schmutzgeier@users.noreply.github.com>
2026-05-05 19:19:35 +0000

    reduce timeout to 500ms

--- esphome/components/flexit_modbus_server/flexit_modbus_server.h
+++ esphome/components/flexit_modbus_server/flexit_modbus_server.h
@@ -314,7 +314,7 @@ private:
   bool tx_enable_direct_{true};
 
   /// @brief How long (ms) to hold a command coil set after send_cmd().
-  static constexpr uint32_t CMD_COIL_TIMEOUT_MS = 10000;
+  static constexpr uint32_t CMD_COIL_TIMEOUT_MS = 5000;
 
   /// @brief Maximum number of simultaneously pending commands.
   static constexpr uint8_t MAX_PENDING_CMDS = 16;
